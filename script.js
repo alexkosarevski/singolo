f2db8c95b6a19956e37802452f47ae908674a77f
alexkosarevski <kasarevich1994@yandex.ru>
2020-03-16 21:18:41 +0300

    feat: add header offset

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ window.onload = function () {
 const addNavClickHandler = () => {
   const navTab = document.getElementById('navigation');
   navTab.addEventListener('click', (event) => {
+    // отменяю стандартный переход по ссылке, чтобы задать отступ для фиксированного хэдера
+    event.preventDefault();
+    let id = event.target.getAttribute('href').slice(1);
+    let section = document.getElementById(id).getBoundingClientRect().top + pageYOffset - 95;
+    window.scrollTo(0, section);
     navTab.querySelectorAll('a').forEach(el => el.classList.remove('navigation__item_active'));
     event.target.classList.add('navigation__item_active');
   });
