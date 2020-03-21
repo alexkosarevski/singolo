24d61fdd1858e178ce71e7f23f0dea76dffaa340
alexkosarevski <kasarevich1994@yandex.ru>
2020-03-22 00:53:34 +0300

    feat: add Phone Click Handler

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 window.onload = function () {
 
   addNavClickHandler();
+  addPhoneClickHandler();
 }
 
 const addNavClickHandler = () => {
-  const navTab = document.getElementById('navigation');
-  navTab.addEventListener('click', (event) => {
-    // отменяю стандартный переход по ссылке, чтобы задать отступ для фиксированного хэдера
-    event.preventDefault();
-    let id = event.target.getAttribute('href').slice(1);
-    let section = document.getElementById(id).getBoundingClientRect().top + pageYOffset - 95;
-    window.scrollTo(0, section);
-    navTab.querySelectorAll('a').forEach(el => el.classList.remove('navigation__item_active'));
-    event.target.classList.add('navigation__item_active');
+  document.addEventListener('scroll', onScroll => {
+    const currentPos = window.scrollY;
+    const sections = document.querySelectorAll('main > section');
+    const links = document.querySelectorAll('#navigation > a');
+    console.log(sections);
+    console.log(links);
+    sections.forEach((el) => {
+      if (el.offsetTop <= currentPos && (el.offsetTop + el.offsetHeight) > currentPos) {
+        links.forEach((link) => {
+          link.classList.remove('navigation__item_active');
+          if (el.getAttribute('id') === link.getAttribute('href').substring(1)) {
+            link.classList.add('navigation__item_active');
+          }
+        })
+      }
+    });
+  });
+}
+
+const addPhoneClickHandler = () => {
+  const phones = document.querySelectorAll('.phone__image');
+  const HIDDEN = "phone__screen_hidden";
+  phones.forEach((phone) => {
+    phone.addEventListener('click', onClick => {
+      const img = phone.nextElementSibling;
+      console.log(img.classList);
+      img.classList.contains(HIDDEN) ? img.classList.remove(HIDDEN) : img.classList.add(HIDDEN);
+    });
   });
 }
